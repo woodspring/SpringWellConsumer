12a6d722d8b540936de275193a0c869e94cccd8c
woodspring <woodspring.toronto@gmail.com>
2021-11-13 20:18:23 -0500

    start project

---
+++ src/main/java/woodspring/springwellconsumer/SpringWellConsumerApplication.java
@@ -0,0 +1,13 @@
+package woodspring.springwellconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWellConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWellConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/woodspring/springwellconsumer/SpringWellConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package woodspring.springwellconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWellConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
